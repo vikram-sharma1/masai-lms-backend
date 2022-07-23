12026a71d3c9809a9ea6f2d8a8b7cf5f65ccd3c2
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-07-23 15:39:52 +0530

    pagination

--- src/controller/lecture.controller.js
+++ src/controller/lecture.controller.js
@@ -22,8 +22,9 @@ router.post("",async(req,res) => {
 
 router.get("",async(req,res) => {
     try {
-      
-        const user = await Lecture.find().lean().exec()
+        const page = req.query.page || 1;
+        const size = req.query.size || 5;
+        const user = await Lecture.find().skip((page-1)*size).limit(size).lean().exec()
         return res.send(user)
 
     } catch (error) {
